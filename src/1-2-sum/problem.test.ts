4eb3943b6b6c9b3dbcc3e598b210f9a4b83d06f1
redaphid <iam@aaronherres.com>
2022-01-16 02:29:59 -0700

    just commit a jest expectation example for unsorted arrays

--- src/1-2-sum/problem.test.ts
+++ src/1-2-sum/problem.test.ts
@@ -2,7 +2,7 @@ import { twoSum } from "./problem";
 test("Example 1", () => {
   const nums = [2, 7, 11, 15]
   const target = 9;
-  expect(twoSum(nums, target)).toEqual([0, 1]);
+  expect(twoSum(nums, target)).toEqual(expect.arrayContaining([0, 1]));
 })
 
 test("Example 2", () => {
