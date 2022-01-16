cc7a746b1418a3ead99c95afcd6f8cd6d7f46fde
redaphid <iam@aaronherres.com>
2022-01-16 16:12:59 -0700

    easy peasy

---
+++ src/interview-questions/rotate-array/problem.ts
@@ -0,0 +1,10 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], rotateBy: number): void {
+  for(let i=0; i < rotateBy; i++) {
+    nums.unshift(nums.pop()!);
+  }
+};
+
+export { rotate };
