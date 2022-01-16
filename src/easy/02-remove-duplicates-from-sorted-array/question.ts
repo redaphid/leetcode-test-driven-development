b73a33dd4f38d1599578b9009a15b0a3d5905d29
redaphid <iam@aaronherres.com>
2022-01-16 16:58:11 -0700

    base case 1

--- src/easy/02-remove-duplicates-from-sorted-array/question.ts
+++ src/easy/02-remove-duplicates-from-sorted-array/question.ts
@@ -1,5 +1,8 @@
 function removeDuplicates(nums: number[]): number {
-  
+  let rightBound=-1, leftBound=-1, current=-1
+  for(let i = 0; i < nums.length; i++) {
+    nums[1]=2
+  }
   return 2
 };
 
