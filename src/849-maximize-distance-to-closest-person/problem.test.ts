e2b5d89ce78154e3277d4d4b2193383567625c87
redaphid <iam@aaronherres.com>
2022-01-15 23:20:31 -0700

    made a really chatty algorithm

--- src/849-maximize-distance-to-closest-person/problem.test.ts
+++ src/849-maximize-distance-to-closest-person/problem.test.ts
@@ -16,3 +16,15 @@ test("Example 3", () => {
   const result = maxDistToClosest(seats);
   expect(result).toBe(1);
 })
+
+test("Example 4", () => {
+ const seats = [0,0,1]
+  const result = maxDistToClosest(seats)
+  expect(result).toBe(2)
+})
+
+test ("Example 5", () => {
+ const seats =  [1,0,0,1]
+ const result = maxDistToClosest(seats)
+ expect(result).toBe(1)
+})
--- src/849-maximize-distance-to-closest-person/problem.ts
+++ src/849-maximize-distance-to-closest-person/problem.ts
@@ -1,29 +1,38 @@
 function maxDistToClosest(seats: number[]): number {
-  let bestDist = -1;
-  let firstOccupiedSeat = -1
-  let lastOccupiedSeat = 0
-  let currentDist = 0
-  
-  let bestStartingSeat = -1
-  let bestEndingSeat = -1
-  seats.forEach((occupied, seat) => {   
-    if(!occupied) return currentDist++   
-    lastOccupiedSeat = seat
-    if(currentDist > bestDist) {      
-      bestStartingSeat = firstOccupiedSeat
-      bestEndingSeat = lastOccupiedSeat
+  let lastOccupiedSeat = -1
+  let lastBestOccupiedSeat = -1
+  let bestDist = 0
 
-      bestDist = currentDist
-      firstOccupiedSeat = seat     
+  seats.forEach((occupied, currentSeat) => {
+    if (!occupied) return;
+    const seatMap = [...new Array(seats.length).fill(' ')]
+    //Ok, let's think about the people brackets now.
+    console.log(`${currentSeat} is occupied. the last occupied seat was ${lastOccupiedSeat}`)
+    seatMap[currentSeat] = '^'
+    seatMap[lastOccupiedSeat] = '^'
+    console.table({seats, seatMap})
+
+    let distance = (currentSeat - lastOccupiedSeat) -1;
+    
+    console.log(`the distance between the last occupied seat (${lastOccupiedSeat}) and ${currentSeat} is ${distance}`)
+    lastOccupiedSeat = currentSeat
+    if (distance > bestDist) {
+      console.log(`this is the best distance so far. last best distance was ${bestDist}`)
+      bestDist = distance;
+      lastBestOccupiedSeat = currentSeat
     }
-    lastOccupiedSeat = seat
-    currentDist = 0
-  });
-  currentDist = seats.length - lastOccupiedSeat
-  if (currentDist > bestDist) {
-   return seats.length - 1
-  }
-  return Math.round((bestEndingSeat - bestStartingSeat)/2)
+  })
+  const begginingSeat = lastBestOccupiedSeat - bestDist -1
+  console.log(`I've decided that the best distance is ${bestDist}, between seats ${begginingSeat} and ${lastBestOccupiedSeat}`)
+  const seatMap = [...new Array(seats.length).fill(' ')]
+  seatMap[begginingSeat] = '^'  
+  seatMap[lastBestOccupiedSeat] = '^'
+  const alexPosition = Math.round((lastBestOccupiedSeat - begginingSeat)/2)
+  seatMap[alexPosition] = 'A'
+  console.table({seats, seatMap})
+  const alexDistance = alexPosition - begginingSeat
+  console.log(`the distance from Alex to the nearest person is: ${alexDistance}, otherwise known as ${bestDist}`)
+  return alexDistance
 }
 
-export {maxDistToClosest}
+export { maxDistToClosest };
