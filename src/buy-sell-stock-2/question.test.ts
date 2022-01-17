19cffbbb3bca0604dd9d35596c6195dcba082973
redaphid <iam@aaronherres.com>
2022-01-16 23:10:24 -0700

    I'm over this

--- src/buy-sell-stock-2/question.test.ts
+++ src/buy-sell-stock-2/question.test.ts
@@ -1,12 +1,21 @@
-
-    import {maxProfit} from './solution'
-    describe('maxProfit', () => {
-      test("Example 1", () => {
-        const prices = [7,1,5,3,6,4]
-        const bestPrice = 7
-        const result = maxProfit(prices)
-        expect(result).toEqual(bestPrice)
-      })
-    })
-    
-  
+import { maxProfit } from "./solution";
+describe("maxProfit", () => {
+  test("Example 1", () => {
+    const prices = [7, 1, 5, 3, 6, 4];
+    const bestPrice = 7;
+    const result = maxProfit(prices);
+    expect(result).toEqual(bestPrice);
+  });
+  test("Example 2", () => {
+    const prices = [1, 2, 3, 4, 5];
+    const bestPrice = 4;
+    const result = maxProfit(prices);
+    expect(result).toEqual(bestPrice);
+  });
+  test("Example 3", () => {
+    const prices = [7, 6, 4, 3, 1];
+    const bestPrice = 0;
+    const result = maxProfit(prices);
+    expect(result).toEqual(bestPrice);
+  });
+});
--- src/buy-sell-stock-2/solution.ts
+++ src/buy-sell-stock-2/solution.ts
@@ -1,25 +1,18 @@
 function maxProfit(prices: number[]): number {
-  let buyDay = 0;
-  let sellDay = 0;
-  let bestProfit = 0;
-  let longest = 0;
-  for (let d1 = 0; d1 < prices.length; d1++) {
-    for (let d2 = d1; d2 < prices.length; d2++) {
-      const p1 = prices[d1];
-      const p2 = prices[d2];
-      const profit = p2 - p1;
-      if (profit <= 0) continue;
-
-      const length = d2 - d1;
-      if (length < longest) continue;
-      buyDay = d1;
-      sellDay = d2;
-      bestProfit = p2 - p1;
-      longest = length
+  if(prices.length === 0) return 0;
+    let startPrice = prices[0];
+    for(let i = 0; i < prices.length; i++) {
+        if(prices[i] < startPrice) {
+            startPrice = prices[i];
+        }
+        
     }
-  }
-  console.table({buyDay,sellDay,bestProfit, longest});
-  if(longest === 0) return bestProfit
-  return bestProfit + maxProfit(prices.slice(buyDay +1, sellDay+1));
+    let endPrice = prices[prices.length - 1];
+    for(let i = prices.length - 1; i >= 0; i--) {
+        if(prices[i] > endPrice) {
+            endPrice = prices[i];
+        }
+    }
+    return endPrice - startPrice;
 }
 export { maxProfit };
