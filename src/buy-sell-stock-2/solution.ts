6645725a025d9bacc1430887f3ac4015e7106485
redaphid <iam@aaronherres.com>
2022-01-16 22:42:43 -0700

    it is pretty wacky. bout to get simpler, though

--- src/buy-sell-stock-2/solution.ts
+++ src/buy-sell-stock-2/solution.ts
@@ -8,11 +8,13 @@ type Gain = {
 
 function maxProfit(prices: number[]): number {
   const gainz = findGainz(prices)
+  if(gainz.length === 0) return 0
   const longestPeriod = gainz.sort((a,b) => b.length - a.length)[0]
-  if(longestPeriod.profit < 0){
-    return 0
-  }
-  console.table(gainz)
+  const smallerGainz = findGainz(prices.slice(
+    longestPeriod.d1,
+    longestPeriod.d2,
+    ))
+  console.table({gainz, smallerGainz})
   return 0
 }
   
@@ -30,8 +32,7 @@ for(let d1 = 0; d1 < prices.length; d1++){
               d2,
               profit,
               length: d2 - d1,
-            }
-            console.log(`I could buy on day ${gain.d1} and sell on ${gain.d2}, for a profit of ${gain.profit}`)
+            }          
             gainz.push(gain)
         }
     }
