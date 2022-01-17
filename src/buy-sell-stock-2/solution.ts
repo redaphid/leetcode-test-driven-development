a7c13fdfd59eb50059246ee0905a5aa04cb3e101
redaphid <iam@aaronherres.com>
2022-01-16 22:39:27 -0700

    commit this wackyness until I go for a recursive approach

--- src/buy-sell-stock-2/solution.ts
+++ src/buy-sell-stock-2/solution.ts
@@ -1,15 +1,46 @@
-function maxProfit(prices: number[]): number {  
-  for(let d1 = 0; d1 < prices.length; d1++){
-      for(let d2 = d1; d2 < prices.length; d2++ ){
-          const p1 = prices[d1]
-          const p2 = prices[d2]
-          if(p1 < p2){
-              console.log(`I could buy on day ${d1} and sell on ${d2}, for a profit of ${p2-p1}`)
-          }
-      }
+
+type Gain = {
+  d1: number,
+  d2: number,
+  profit: number,
+  length: number
+}
+
+function maxProfit(prices: number[]): number {
+  const gainz = findGainz(prices)
+  const longestPeriod = gainz.sort((a,b) => b.length - a.length)[0]
+  if(longestPeriod.profit < 0){
+    return 0
   }
+  console.table(gainz)
   return 0
 }
+  
+
+function findGainz(prices: number[]): Gain[] {
+let gainz: any[] = []
+for(let d1 = 0; d1 < prices.length; d1++){
+    for(let d2 = d1; d2 < prices.length; d2++ ){
+        const p1 = prices[d1]
+        const p2 = prices[d2]
+        const profit = p2 - p1
+        if(profit > 0){
+            const gain = {
+              d1,
+              d2,
+              profit,
+              length: d2 - d1,
+            }
+            console.log(`I could buy on day ${gain.d1} and sell on ${gain.d2}, for a profit of ${gain.profit}`)
+            gainz.push(gain)
+        }
+    }
+}
+return gainz
+}
+
+
+
 export {maxProfit}
 
   
