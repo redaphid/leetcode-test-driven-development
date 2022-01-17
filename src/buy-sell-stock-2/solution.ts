2d084f98ebbacc12cdec2a1635f6bdd54d70e63d
redaphid <iam@aaronherres.com>
2022-01-16 22:54:32 -0700

    no idea how to do this

--- src/buy-sell-stock-2/solution.ts
+++ src/buy-sell-stock-2/solution.ts
@@ -1,47 +1,25 @@
-
-type Gain = {
-  d1: number,
-  d2: number,
-  profit: number,
-  length: number
-}
-
 function maxProfit(prices: number[]): number {
-  const gainz = findGainz(prices)
-  if(gainz.length === 0) return 0
-  const longestPeriod = gainz.sort((a,b) => b.length - a.length)[0]
-  const smallerGainz = findGainz(prices.slice(
-    longestPeriod.d1,
-    longestPeriod.d2,
-    ))
-  console.table({gainz, smallerGainz})
-  return 0
-}
-  
+  let buyDay = 0;
+  let sellDay = 0;
+  let bestProfit = 0;
+  let longest = 0;
+  for (let d1 = 0; d1 < prices.length; d1++) {
+    for (let d2 = d1; d2 < prices.length; d2++) {
+      const p1 = prices[d1];
+      const p2 = prices[d2];
+      const profit = p2 - p1;
+      if (profit <= 0) continue;
 
-function findGainz(prices: number[]): Gain[] {
-let gainz: any[] = []
-for(let d1 = 0; d1 < prices.length; d1++){
-    for(let d2 = d1; d2 < prices.length; d2++ ){
-        const p1 = prices[d1]
-        const p2 = prices[d2]
-        const profit = p2 - p1
-        if(profit > 0){
-            const gain = {
-              d1,
-              d2,
-              profit,
-              length: d2 - d1,
-            }          
-            gainz.push(gain)
-        }
+      const length = d2 - d1;
+      if (length < longest) continue;
+      buyDay = d1;
+      sellDay = d2;
+      bestProfit = p2 - p1;
+      longest = length
     }
+  }
+  console.table({buyDay,sellDay,bestProfit, longest});
+  if(longest === 0) return bestProfit
+  return bestProfit + maxProfit(prices.slice(buyDay +1, sellDay+1));
 }
-return gainz
-}
-
-
-
-export {maxProfit}
-
-  
+export { maxProfit };
