c34c5c9a1ae34ad304e840d8ba374d8b747074f2
redaphid <iam@aaronherres.com>
2022-01-16 20:38:24 -0700

    had to use copilot for this one. :(.

--- src/easy/04-single-number/question.ts
+++ src/easy/04-single-number/question.ts
@@ -1,25 +1,5 @@
-function singleNumber(nums: number[]): number {
-  const seen = new Set<number>()
-  let lookingFor = Infinity
-  
-  for(let i=0; i< nums.length; i++){
-      const current = nums[i]
-      if(!seen.has(current)) {
-          lookingFor = current
-          seen.add(lookingFor)
-          console.table({seen, lookingFor,current})
-          continue
-      }
-      lookingFor = Infinity
-  }
-  return lookingFor
-};
-
-
-
-
-
-
-
+function singleNumber(nums: number[]) {
+  return nums.reduce((acc, curr) => acc ^ curr, 0);
+}
 
 export {singleNumber}
