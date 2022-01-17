0663d122c4716ec44e631d8a1502b1ef80c6b27e
redaphid <iam@aaronherres.com>
2022-01-16 21:14:44 -0700

    copilot did this, with a little help from me. Pretty ugly, though

--- src/easy/05-intersection-of-2-arrays/question.ts
+++ src/easy/05-intersection-of-2-arrays/question.ts
@@ -1,28 +1,25 @@
 function intersect(nums1:number[], nums2:number[]):number[] {
   const map1 = new Map<number, number>();
-  const map2 = new Map<number, number>();  
+  const map2 = new Map<number, number>();    
+  const maxLength = Math.max(nums1.length, nums2.length);
+  for(let i=0; i < maxLength; i++) {
+    if(i < nums1.length) {
+      map1.set(nums1[i], (map1.get(nums1[i]) || 0) + 1);
+    }
+    if(i < nums2.length) {
+      map2.set(nums2[i], (map2.get(nums2[i]) || 0) + 1);
+    }
+  }
   let result:number[] = [];
-  for (let i = 0; i < nums1.length; i++) {
-    if (map1.has(nums1[i])) {
-      map1.set(nums1[i], map1.get(nums1[i])! + 1);
-    } else {
-      map1.set(nums1[i], 1);
+  for(let [k,timesSeen] of  map1.entries()) {
+    const timesSeen2 =  map2.get(k)
+    if(!timesSeen2) {
+      continue;
     }
+    let minTimesSeen = Math.min(timesSeen, timesSeen2);
+    result = [...result, ...new Array(minTimesSeen).fill(k)];
   }
-  for (let i = 0; i < nums2.length; i++) {
-    if (map2.has(nums2[i])) {
-      map2.set(nums2[i], map2.get(nums2[i])! + 1);
-    } else {
-      map2.set(nums2[i], 1);
-    }
-  }
-  for (let i = 0; i < nums1.length; i++) {
-    if (map2.has(nums1[i]) && map1.get(nums1[i])! <= map2.get(nums1[i])!) {
-      result.push(nums1[i]);
-    }
-  }
-
-  return result
+  return result;
 }
 
 export {intersect}
