642d85b3cc2015034eb9b0ac6e8124a26db630e6
redaphid <iam@aaronherres.com>
2022-01-17 22:33:05 -0700

    valid palindrome

---
+++ src/valid-palindrome/question.test.ts
@@ -0,0 +1,9 @@
+
+    import {validpalindrome} from './solution'
+    describe('validpalindrome', () => {
+      test("Example 1", () => {
+        expect(true).toBe(false)
+      })
+    })
+    
+  
---
+++ src/valid-palindrome/solution.ts
@@ -0,0 +1,7 @@
+
+  function validpalindrome() {
+    throw new Error("This solution has not been implemented yet.")
+  }
+  export {validpalindrome}
+
+  
