979eec189df854fe0ca66178c783e36ee876f59a
redaphid <iam@aaronherres.com>
2022-01-17 22:29:42 -0700

    think the string anagram problem is done... But it's supposed to be easy, and it was still 15m for me :(

--- src/valid-anagram/question.test.ts
+++ src/valid-anagram/question.test.ts
@@ -1,9 +1,15 @@
-
-    import {validanagram} from './solution'
-    describe('validanagram', () => {
-      test("Example 1", () => {
-        expect(true).toBe(false)
-      })
-    })
-    
-  
+import { isAnagram } from "./solution";
+describe("isAnagram", () => {
+  test("Example 1", () => {
+    const s = "anagram";
+    const t = "nagaram";
+    const result = isAnagram(s, t);
+    expect(result).toBe(true);
+  });
+  test("Example 2", () => {
+    const s = "rat";
+    const t = "car";
+    const result = isAnagram(s, t);
+    expect(result).toBe(false);
+  });
+});
--- src/valid-anagram/solution.ts
+++ src/valid-anagram/solution.ts
@@ -1,7 +1,22 @@
-
-  function validanagram() {
-    throw new Error("This solution has not been implemented yet.")
+function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+  const indexChars = (s: string): Map<string, number> => {
+    const sMap = new Map<string, number>();
+    for (let i = 0; i < s.length; i++) {
+      const c = s[i];
+      let times = sMap.get(c) || 0;
+      sMap.set(c, times + 1);
+    }
+    return sMap;
+  };
+  const sMap = indexChars(s);
+  const tMap = indexChars(t);
+  for (let [k, v] of sMap.entries()) {
+    if (tMap.get(k) !== v) {
+      return false;
+    }
   }
-  export {validanagram}
+  return true;
+}
 
-  
+export { isAnagram };
