a70a8317b694cdf17fd7e307a8a4ae8b5b2d4cc9
redaphid <iam@aaronherres.com>
2022-01-19 23:59:32 -0700

    it's cheesy, but it works(?)

--- src/symmetric-tree/question.test.ts
+++ src/symmetric-tree/question.test.ts
@@ -13,7 +13,7 @@
         const result = isSymmetric(input)
         expect(result).toBe(true)
       })
-      test.only("Example 2", () => {
+      test("Example 2", () => {
         //input = [1,2,2,null,3,null,3]
         const input = {
           val: 1,
@@ -22,6 +22,39 @@
           },
           right: { val: 2, left: { val: 3, }},
         }
+        const result = isSymmetric(input)
+        expect(result).toBe(false)
+      })
+      test("Example 3", () => {
+        //input = [1,2,2,3,4,4,3]
+        const input = {
+          val: 1,
+          left: { val: 2, 
+                  left: { val: 3, },  
+                  right: { val: 4, },
+          },
+          right: { val: 2,
+                   left: { val: 3, },
+                    right: { val: 4, },
+
+          }
+        }         
+        const result = isSymmetric(input)
+        expect(result).toBe(true)
+      })
+      test("Example 4", () => {        
+        const input = {
+          val: 1,
+          left: { val: 2, 
+                  left: { val: 3, },  
+                  right: { val: 9, },
+          },
+          right: { val: 2,
+                   left: { val: 3, },
+                    right: { val: 4, },
+
+          }
+        }         
         const result = isSymmetric(input)
         expect(result).toBe(false)
       })
--- src/symmetric-tree/solution.ts
+++ src/symmetric-tree/solution.ts
@@ -4,15 +4,27 @@ export interface TreeNode {
   right?: TreeNode | null;
 }
 
-const valsMatch = (a: TreeNode | null | undefined, b: TreeNode | null | undefined) => {
-  if(a && !b) return false;
-  if(!a && b) return false;
-  return a?.val === b?.val;
+const LSHIFT = 100000
+const RSHIFT = Math.pow(LSHIFT, 10)
+
+const getTreeVal = (depth: number, node?: TreeNode | null): number => {
+  if (!node) return 0;
+  if(!branchesBalanced(node.left, node.right)) throw new Error("Branches not balanced");
+  return node.val + getTreeVal(depth * LSHIFT, node.left) + getTreeVal(depth *RSHIFT, node.right);
+}
+
+const branchesBalanced = (a: TreeNode | null | undefined, b: TreeNode | null | undefined) => {
+  if (a && !b) return false;
+  if (!a && b) return false;
+  return true
 }
 
 function isSymmetric(root: TreeNode | null): boolean {
-  if(root === null) return true;
-  if(!valsMatch(root.left, root.right)) return false;
-  return isSymmetric(root.left || null) && isSymmetric(root.right || null);
+  if (root === null) return true;
+  try {
+  return getTreeVal(1,root.left) === getTreeVal(1,root.right)
+  } catch(e) {
+    return false
+  }
 }
 export { isSymmetric };
