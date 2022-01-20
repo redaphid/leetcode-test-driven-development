6d520a1bbda600f91280a2227db2d36f0fb1f20a
redaphid <iam@aaronherres.com>
2022-01-19 22:29:53 -0700

    I'm actually relieved this didn't work first try. #justcopilotthings

--- src/validate-binary-tree/solution.ts
+++ src/validate-binary-tree/solution.ts
@@ -5,7 +5,12 @@ export interface TreeNode {
 }
 
 function isValidBST(root: TreeNode | null): boolean {
-  return true;
+  if (!root) return true
+  if(!root.left && !root.right) return true
+  const {val} = root
+  if(root.left && root.left.val >= val) return false
+  if(root.right && root.right.val <= val) return false
+  return isValidBST(root.left || null) && isValidBST(root.right || null)
 }
 
 export { isValidBST };
