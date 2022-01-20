81c483991f6a3734950aca6f64322a44f18fc49b
redaphid <iam@aaronherres.com>
2022-01-19 23:27:36 -0700

    why did typescript let me just put 'false' in the middle of a block without any assignment or conditional????

--- src/symmetric-tree/solution.ts
+++ src/symmetric-tree/solution.ts
@@ -5,7 +5,6 @@ export interface TreeNode {
 }
 
 const valsMatch = (a: TreeNode | null | undefined, b: TreeNode | null | undefined) => {
-  console.log({a, b});  
   if(a && !b) return false;
   if(!a && b) return false;
   return a?.val === b?.val;
@@ -13,8 +12,7 @@ const valsMatch = (a: TreeNode | null | undefined, b: TreeNode | null | undefine
 
 function isSymmetric(root: TreeNode | null): boolean {
   if(root === null) return true;
-  if(!valsMatch(root.left, root.right)) false;
-
+  if(!valsMatch(root.left, root.right)) return false;
   return isSymmetric(root.left || null) && isSymmetric(root.right || null);
 }
 export { isSymmetric };
