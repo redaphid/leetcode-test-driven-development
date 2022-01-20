7ff85b5e0d7f46ecc9b81304456f534ac52c0e1f
redaphid <iam@aaronherres.com>
2022-01-19 22:14:44 -0700

    maybe leaned a little toooo much on copilot this time. But it was easy

--- src/max-tree-depth/question.test.ts
+++ src/max-tree-depth/question.test.ts
@@ -1,9 +1,23 @@
-
-    import {maxDepth} from './solution'
-    describe('maxDepth', () => {
-      test("Example 1", () => {
-        expect(true).toBe(false)
-      })
-    })
-    
-  
+import { TreeNode, maxDepth } from "./solution";
+describe("maxDepth", () => {
+  test("Example 1", () => {
+    // [3,9,20,null,null,15,7]
+    const root: TreeNode = {
+      val: 3,
+      left: {
+        val: 9,
+      },
+      right: {
+        val: 20,
+        left: {
+          val: 15,
+        },
+        right: {
+          val: 7,
+        },
+      },
+    };
+    const result = maxDepth(root);
+    expect(result).toBe(3);
+  });
+});
--- src/max-tree-depth/solution.ts
+++ src/max-tree-depth/solution.ts
@@ -1,7 +1,21 @@
+export interface TreeNode {
+  val: number;
+  left?: TreeNode | null;
+  right?: TreeNode | null;
+}
 
-  function maxDepth() {
-    throw new Error("This solution has not been implemented yet.")
-  }
-  export {maxDepth}
-
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0
   
+  const walk = (n)=>{
+    if (!n.left && !n.right) return 1
+    let left = 0
+    let right = 0
+    if (n.left) left = walk(n.left)
+    if (n.right) right = walk(n.right)
+    return Math.max(left, right) + 1
+  }
+
+  return walk(root)
+}
+export { maxDepth };
