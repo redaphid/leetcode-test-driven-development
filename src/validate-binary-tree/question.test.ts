7ea4aa8e47b9b2cfb2f6f41c8dd74abafe69ab58
redaphid <iam@aaronherres.com>
2022-01-19 22:21:19 -0700

    example 2 test

--- src/validate-binary-tree/question.test.ts
+++ src/validate-binary-tree/question.test.ts
@@ -1,15 +1,24 @@
-
-    import {isValidBST} from './solution'
-    describe('isValidBST', () => {
-      test("Example 1", () => {
-        const input = {
-          val: 2,
-          left: {val: 1},
-          right: {val: 3},
-        }
-        const result = isValidBST(input)
-        expect(result).toBe(true)
-      })
-    })
-    
-  
+import { isValidBST } from "./solution";
+describe("isValidBST", () => {
+  test("Example 1", () => {
+    const input = {
+      val: 2,
+      left: { val: 1 },
+      right: { val: 3 },
+    };
+    const result = isValidBST(input);
+    expect(result).toBe(true);
+  });
+  test("Example 2", () => {
+    const input = {
+      val: 5,
+      left: { val: 1 },
+      right: { 
+        val: 4, 
+        left: { val: 3 }, 
+        right: { val: 6 } },
+    };
+    const result = isValidBST(input);
+    expect(result).toBe(false);
+  });
+});
