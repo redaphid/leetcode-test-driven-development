f66a05a443cabbe6986c6c0836e02150fb788491
redaphid <iam@aaronherres.com>
2022-01-19 21:53:51 -0700

    example test

--- src/reverse-list/question.test.ts
+++ src/reverse-list/question.test.ts
@@ -1,7 +1,15 @@
 
     import {reverseList} from './solution'
+    import {arrayToList} from '../util/linked-lists'
+    
     describe('reverseList', () => {
       test("Example 1", () => {
+        const input = arrayToList([1,2,3,4,5])
+        const output = arrayToList([5,4,3,2,1])
+        
+        const result = arrayToList(reverseList(input))
+
+        expect(result).toEqual(output)
         expect(true).toBe(false)
       })
     })
