9a6edf86f573e34ff6bcfacec753287233587fa6
redaphid <iam@aaronherres.com>
2022-01-19 23:04:40 -0700

    symmetric tree tests

---
+++ src/symmetric-tree/question.test.ts
@@ -0,0 +1,30 @@
+
+    import {isSymmetric} from './solution'
+    describe('isSymmetric', () => {
+      test("Example 1", () => {
+        // [1,2,2,3,4,4,3]
+        //  
+        const input = {
+          val: 1,
+          left: { val: 2,  },
+          right: { val: 2},
+              
+        }
+        const result = isSymmetric(input)
+        expect(result).toBe(true)
+      })
+      test("Example 2", () => {
+        //input = [1,2,2,null,3,null,3]
+        const input = {
+          val: 1,
+          left: { val: 2,
+                  left: { val: 3, },
+          },
+          right: { val: 2, left: { val: 3, }},
+        }
+        const result = isSymmetric(input)
+        expect(result).toBe(false)
+      })
+    })
+    
+  
---
+++ src/symmetric-tree/solution.ts
@@ -0,0 +1,10 @@
+export interface TreeNode {
+  val: number;
+  left?: TreeNode | null;
+  right?: TreeNode | null;
+}
+
+function isSymmetric(root: TreeNode | null): boolean {
+  return true;
+}
+export { isSymmetric };
