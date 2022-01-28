a60928ed519383448efdaa753bb0936cbb23c2b2
redaphid <iam@aaronherres.com>
2022-01-27 22:29:31 -0700

    finally get to use sompething simple

--- src/merge-sorted-array/question.test.ts
+++ src/merge-sorted-array/question.test.ts
@@ -4,6 +4,13 @@
       test("Existence", () => {
         expect(merge).toBeDefined()
       })
+      test("Example 1", () => {
+        const nums1 = [1,2,3,0,0,0]
+        const nums2 = [2,5,6]
+        const m = 3
+        merge(nums1, m, nums2, 3)
+        expect(nums1).toEqual([1,2,2,3,5,6])
+      })
     })
     
   
--- src/merge-sorted-array/solution.ts
+++ src/merge-sorted-array/solution.ts
@@ -1,5 +1,7 @@
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-
-};
-
+  const combined = [...nums1.slice(0,m), ...nums2].sort()
+  for(let i = 0; i < combined.length; i++) {
+    nums1[i] = combined[i]
+  }
+}
 export {merge}
