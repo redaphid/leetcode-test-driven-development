564470b99beeeedbf2d92240ba8219a44b5b4d3c
redaphid <iam@aaronherres.com>
2022-01-27 22:31:03 -0700

    guess javascript's sorting is string-based by default(?)

--- src/merge-sorted-array/solution.ts
+++ src/merge-sorted-array/solution.ts
@@ -1,5 +1,8 @@
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-  const combined = [...nums1.slice(0,m), ...nums2].sort()
+  const combined = [
+    ...nums1.slice(0,m),
+    ...nums2
+  ].sort((a,b) => a-b)
   for(let i = 0; i < combined.length; i++) {
     nums1[i] = combined[i]
   }
