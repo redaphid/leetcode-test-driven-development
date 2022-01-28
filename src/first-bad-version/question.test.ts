6d23ef7c72e1968303b82764346bd270263a4993
redaphid <iam@aaronherres.com>
2022-01-27 23:18:47 -0700

    done w/ an extremely memory-hungry solution

--- src/first-bad-version/question.test.ts
+++ src/first-bad-version/question.test.ts
@@ -8,8 +8,27 @@ describe("firstbadversion", () => {
   });
   test("Example 2", () => {
     const n = 5
-    const solution = firstbadversion(jest.fn().mockReturnValue(false));
+    const solution = firstbadversion((version)=>{
+      console.log({version});
+      return (version > 3)
+    });
 
-    expect(solution(n)).toEqual(5);
+    expect(solution(n)).toEqual(4);
   });
+  test("Example 3", () => {
+    const n = 2
+    const solution = firstbadversion((version)=>{
+      console.log({version});
+      return (version > 1)
+    })
+    expect(solution(n)).toEqual(2);
+  })
+  test("Example 4", () => {
+    const n = 4
+    const solution = firstbadversion((version)=>{
+      console.log({version});
+      return (version >2)
+    })
+    expect(solution(n)).toEqual(3);
+  })
 });
--- src/first-bad-version/solution.ts
+++ src/first-bad-version/solution.ts
@@ -1,10 +1,39 @@
 
+
   type isBadVersion = (version: number) => boolean;
 
-  const  firstbadversion = (isBad:isBadVersion)=>{
-    return (versionCount)=>{
-      return 1
+  const findIt = (versions:number[],isBad:isBadVersion)=>{
+    console.log({versions});
+    if(versions.length === 0) return undefined;
+    if(versions.length === 1) return isBad(versions[0]) ? versions[0] : undefined;
+
+    const midIndex = Math.floor(versions.length/2);
+
+    const version = versions[midIndex];
+    console.log(`isBad(${version}), ${isBad(version)}`);
+    if(isBad(version)){
+      const left = versions.slice(0,midIndex);
+      console.log(`${version} was bad, so we look at the left side. ${left}`);
+      const didFind = findIt(left,isBad);
+      if(didFind !== undefined) return didFind;
+      return version
     }
+    else {
+      const right = versions.slice(midIndex+1);
+      console.log(`${version} was good, so we look at the right side. ${right}`);
+      const didFind = findIt(right,isBad);
+      console.log({didFind})
+
+      return didFind
+    }
+  }
+
+  const  firstbadversion = (isBad:isBadVersion)=>{
+    return (versionCount:number)=>{
+      const versions = new Array(versionCount).fill(0).map((_,i)=>i+1);
+      return findIt(versions,isBad);
+    }
+    
   }
   export {firstbadversion}
 
