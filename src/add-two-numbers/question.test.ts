c93488ebe3e6bab39f38a42231ae66150cdb3688
redaphid <iam@aaronherres.com>
2022-01-30 15:07:04 -0700

    base case w/o carrying

--- src/add-two-numbers/question.test.ts
+++ src/add-two-numbers/question.test.ts
@@ -1,9 +1,13 @@
+import { addTwoNumbers } from "./solution";
+import { listToArray, arrayToList } from "../util/linked-lists";
 
-    import {addTwoNumbers} from './solution'
-    describe('addTwoNumbers', () => {
-      test("Example 1", () => {
-        expect(true).toBe(false)
-      })
-    })
-    
-  
+describe("addTwoNumbers", () => {
+  test("Example 1", () => {
+    const l1 = arrayToList([2, 4, 3]);
+    const l2 = arrayToList([5, 6, 4]);
+    const result = addTwoNumbers(l1, l2);
+    expect(result.val).toEqual(7);
+    expect(result.next.val).toEqual(0);
+    expect(result.next.next.val).toEqual(8);
+  });
+});
--- src/add-two-numbers/solution.ts
+++ src/add-two-numbers/solution.ts
@@ -1,7 +1,15 @@
-
-  function addTwoNumbers() {
-    throw new Error("This solution has not been implemented yet.")
+import { ListNode } from "../util/linked-lists";
+function addTwoNumbers(l1: ListNode, l2: ListNode): ListNode {
+  let carry = 0;
+  let h1 = l1
+  let h2 = l2
+  let l3 = new ListNode()
+  while (  h1 !== null || h2 !== null) {
+    l3.val = h1.val + h2.val
+    l3.next = new ListNode()
+    h1 = h1.next
+    h2 = h2.next    
   }
-  export {addTwoNumbers}
-
-  
+  return l3;
+}
+export { addTwoNumbers };
