b6be77373d90e0b42d625a12f40da9fdf9a7d4ca
redaphid <iam@aaronherres.com>
2022-01-30 14:11:02 -0700

    slowest answer on leetcode

--- src/count-primes/solution.ts
+++ src/count-primes/solution.ts
@@ -1,16 +1,20 @@
-const primeMap = new Map<number, boolean>();
+const primes = new Set<number>();
 const isPrime = (n: number) => {
+  if(primes.has(n)) return true; 
+  const prime =  _isPrime(n);
+  if(prime) primes.add(n);
+  return prime;
+};
+
+const _isPrime = (n: number) => {
   if (n < 2) return false;
   if (n === 2) return true;
-  for (let i = 2; i < n; i++) {
-    if (n % i === 0) {
-      console.log(`${n} is not prime because ${i} is a factor`);
-      return false;
-    }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
   }
-  console.log(`${n} is prime`);
   return true;
 };
+
 function countPrimes(n: number) {
   let count = 0;
   for (let i = 1; i < n; i++) {
