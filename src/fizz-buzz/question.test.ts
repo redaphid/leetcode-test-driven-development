abed057f25f9640886070f5e7b514d4425fdfadb
redaphid <iam@aaronherres.com>
2022-01-30 13:04:20 -0700

    FizzBuzz initial

---
+++ src/fizz-buzz/question.test.ts
@@ -0,0 +1,9 @@
+
+    import {fizzBuzz} from './solution'
+    describe('fizzBuzz', () => {
+      test("Example 1", () => {
+        expect(true).toBe(false)
+      })
+    })
+    
+  
---
+++ src/fizz-buzz/solution.ts
@@ -0,0 +1,7 @@
+
+  function fizzBuzz() {
+    throw new Error("This solution has not been implemented yet.")
+  }
+  export {fizzBuzz}
+
+  
