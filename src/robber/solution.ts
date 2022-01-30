9d4c13cf7568abd193ab265f947fea6d59283c56
redaphid <iam@aaronherres.com>
2022-01-30 01:33:14 -0700

    whelp, it uses all available node memory to do this simple task

--- src/robber/solution.ts
+++ src/robber/solution.ts
@@ -1,28 +1,24 @@
+const getScore = (option:number[], houses:number[]) => {
+  return option.map(i => houses[i]).reduce((a, b) => a + b, 0);
+}
 function rob(houses: number[]): number {
   let viableOptions: Set<number[]> = new Set();
 
+  let score = 0
   const addIfViable = (i: number) => {
     viableOptions.forEach((v) => {
+      score = Math.max(score, getScore(v, houses));
       if (v.includes(i)) return true;
       if (v.includes(i + 1)) return true;
       if (v.includes(i - 1)) return true;
       viableOptions.add([...v, i]);
+      score = Math.max(score, getScore(v, houses));
     });
   };
   for (let i = 0; i < houses.length; i++) {
     viableOptions.add([i]);
     addIfViable(i);
-  }
-  let max = -Infinity;
-  
-  viableOptions.forEach((option) => {
-    const val = option
-      .map((i) => {
-        return houses[i];
-      })
-      .reduce((a, b) => a + b);    
-    max = Math.max(max, val);
-  });
-  return max; // return max
+  }    
+  return score; // return max
 }
 export { rob };
