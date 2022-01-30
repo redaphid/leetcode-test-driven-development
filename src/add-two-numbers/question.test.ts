7836b7694226b50378ddd336cdaf45df3efff340
redaphid <iam@aaronherres.com>
2022-01-30 15:41:52 -0700

    first medium feels like it's more like easy to me

--- src/add-two-numbers/question.test.ts
+++ src/add-two-numbers/question.test.ts
@@ -9,11 +9,19 @@ describe("addTwoNumbers", () => {
     expect(result.val).toEqual(7);
     expect(result.next.val).toEqual(0);
     expect(result.next.next.val).toEqual(8);
+    expect(result.next.next.next).toBeNull();
   });
   test("Example 3", ()=> {
     const l1 = arrayToList([9,9,9,9,9,9,9]);
     const l2 = arrayToList([9,9,9,9]);
     const result = addTwoNumbers(l1, l2);
     expect(result.val).toEqual(8);
+    expect(result.next.val).toEqual(9);
+    expect(result.next.next.val).toEqual(9);
+    expect(result.next.next.next.val).toEqual(9);
+    expect(result.next.next.next.next.val).toEqual(0);
+    expect(result.next.next.next.next.next.val).toEqual(0);
+    expect(result.next.next.next.next.next.next.val).toEqual(0);
+    expect(result.next.next.next.next.next.next.next.val).toEqual(1);
   })
 });
--- src/add-two-numbers/solution.ts
+++ src/add-two-numbers/solution.ts
@@ -7,18 +7,22 @@ function addTwoNumbers(l1: ListNode, l2: ListNode): ListNode {
 
   let carry = 0
   while (  h1 || h2 ) {
+    h3.next = new ListNode()
+    h3 = h3.next
+    
     let sum = (h1?.val || 0) + (h2?.val || 0) + carry
     carry = 0  
     h3.val = sum % 10
     if(h3.val < sum) {
       carry = 1
     }
-    h3.next = new ListNode()
     
     h1 = h1?.next
-    h2 = h2?.next
-    h3 = h3.next    
+    h2 = h2?.next   
   }
-  return l3;
+  if(carry) {
+    h3.next = new ListNode(carry)
+  }
+  return l3.next;
 }
 export { addTwoNumbers };
