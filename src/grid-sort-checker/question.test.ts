85903a0d1b6fba109cb0405152488b405cffbc1c
redaphid <iam@aaronherres.com>
2022-02-06 13:37:41 -0700

    aaand boom goes the dynamite

--- src/grid-sort-checker/question.test.ts
+++ src/grid-sort-checker/question.test.ts
@@ -1,14 +1,35 @@
 
-    import {gridsortchecker} from './solution'
+    import {_gridsortchecker, gridStringToGrid} from './solution'
     describe('gridsortchecker', () => {
       test("Example 1", () => {
         const grid = [
           ['a','b','c'],
-          ['a','d','e'],
+          ['a','d', 'e'],
           ['e','f','g'],
         ]
-        const result = gridsortchecker(grid);
+        const result = _gridsortchecker(grid);
         expect(result).toEqual(true);
+      })
+      test("Example 2", () => {
+        const grid = [
+          ['a','b','c'],
+          ['q','d', 'e'],
+          ['a','e','f'],
+        ]
+        const result = _gridsortchecker(grid);
+        expect(result).toEqual(false);
+      })
+      describe("gridStringToGrid", () => {
+        test("Example 1", () => {
+          const grid = [
+            'abc',
+            'ade',
+            'efg',          
+          ]
+          const result = gridStringToGrid(grid);
+          const row1 = result[0]
+          expect(row1).toEqual(['a','b','c'])
+        })
       })
     })
     
--- src/grid-sort-checker/solution.ts
+++ src/grid-sort-checker/solution.ts
@@ -1,7 +1,21 @@
 
-  function gridsortchecker(grid: string[][]): boolean {
-    return true
+  function _gridsortchecker(grid: string[][]): boolean {
+    for(let row of grid){
+      row = row.sort()    
+    }
+    for(let i =0; i < grid.length; i++){
+      const col = grid.map(row => row[i])
+      const sortedCol = [...col].sort()
+      if(col.join('') !== sortedCol.join('')){
+        return false
+      }
+    }
+    return true 
   }
-  export {gridsortchecker}
+
+  function gridStringToGrid(grid:string[]): string[][]{
+    return grid.map(row => row.split(''))
+  }
+  export {_gridsortchecker, gridStringToGrid}
 
   
