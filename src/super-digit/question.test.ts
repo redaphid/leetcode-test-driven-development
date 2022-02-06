4df96f731bd340d3dfe037619d9e372950bba223
redaphid <iam@aaronherres.com>
2022-02-06 14:16:09 -0700

    before refactor for huge numbers

--- src/super-digit/question.test.ts
+++ src/super-digit/question.test.ts
@@ -1,13 +1,24 @@
 
     import {superdigit} from './solution'
-    describe('superdigit', () => {
-      test("Example 2", () => {       
+    describe.only('superdigit', () => {
+      test.only("Example 2", () => {       
        const result = superdigit('9875',4 )
-       expect(result).toEqual(116)
+       expect(result).toEqual(8)
       })
       test("Example 1", () => {
         const result = superdigit('1', 100)
         expect(result).toEqual(1)
+      })
+      test("When the number is 9875", () => {
+        const result = superdigit('9875', 1)
+        expect(result).toEqual(2)
+      })
+      xtest("When the number is huge", () => {
+        const huge = Math.pow(9, 5)
+          .toString()
+          .repeat(10000)
+        const result = superdigit(huge, Math.pow(9, 5))
+        expect(result).toEqual(2)
       })
     })
     
--- src/super-digit/solution.ts
+++ src/super-digit/solution.ts
@@ -1,6 +1,26 @@
 
+  function addDigits(n: string): number {
+    let result = 0
+    console.log(`input: ${n}`)
+    for(let i = 0; i < n.length; i++) {
+      result += parseInt(n[i])
+    }
+    console.log(`output: ${result}`)
+    return result
+  }
   function superdigit(s:string, k:number) {
+    console.log(`s: ${s}, k: ${k}`)
+    // return 0
     if(s.length === 1) return parseInt(s)
+    let sArray = new Array(k).fill(s)
+    console.log({sArray})
+    let result = sArray.join('')
+    while(result.length > 1) {
+      result = addDigits(result).toString()
+    }
+  
+
+    return parseInt(result)
   }
   export {superdigit}
 
