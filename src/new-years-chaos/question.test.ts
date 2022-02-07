fe88c60be7d7e07f7f132b3088c6ed548fa8e4c5
redaphid <iam@aaronherres.com>
2022-02-06 23:34:36 -0700

    meh. Copilot takes the credit for this one. Didn't learn much from it's 'help'. Yet

--- src/new-years-chaos/question.test.ts
+++ src/new-years-chaos/question.test.ts
@@ -1,9 +1,25 @@
-
-    import {newyearschaos} from './solution'
-    describe('newyearschaos', () => {
-      test("Example 1", () => {
-        expect(true).toBe(false)
-      })
-    })
-    
+import { minBribes } from "./solution";
+describe("minBribes", () => {
+  test("1 bribe", () => {
+    const q = [1,3,2];
+    const bribes = minBribes(q);
+    expect(bribes).toEqual(1);
+  });
+  test("too chaotic", () => {
+    const q = [4,1,2,3];
+    const bribes = minBribes(q);
+    expect(bribes).toEqual(-1);
+  })
+  test("Case 0", () => {
+    const q = [2,1,5,3,4];
+    const bribes = minBribes(q);
+    expect(bribes).toEqual(3);
+  })
   
+  test("Case 1", () => {
+    const q = [5,1,2,3,7,8,6,4,];
+    const bribes = minBribes(q);
+    expect(bribes).toEqual(-1);
+  })
+
+});
--- src/new-years-chaos/solution.ts
+++ src/new-years-chaos/solution.ts
@@ -1,7 +1,17 @@
 
-  function newyearschaos() {
-    throw new Error("This solution has not been implemented yet.")
-  }
-  export {newyearschaos}
 
-  
+function minBribes(q: number[]): number {
+  let bribes = 0;
+  for (let i = 0; i < q.length; i++) {
+    if (q[i] - (i + 1) > 2) {
+      return -1;
+    }
+    for (let j = Math.max(0, q[i] - 2); j < i; j++) {
+      if (q[j] > q[i]) {
+        bribes++;
+      }
+    }
+  }
+  return bribes;
+}
+export { minBribes };
