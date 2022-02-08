90e65daec11145c63132da237ea777927c0430f2
redaphid <iam@aaronherres.com>
2022-02-07 20:54:15 -0700

    commit w/general interface for when I get back from 'a liiiiittle breaky for me. And then we're going to come back. And then I'm going to start the show over'

--- src/q-using-2-stacks/question.test.ts
+++ src/q-using-2-stacks/question.test.ts
@@ -1,15 +1,33 @@
 
-    import {qusing2stacks} from './solution'
-    describe('qusing2stacks', () => {
-      test("Example 1", () => {
-        const input = [
-          "10","1 42","2","1 14","3","1 28","3","1 60","1 78","2","2"
-        ]
-        const expected = [
-          "14", "14"
-        ]
-        expect(qusing2stacks(input)).toEqual(expected)
-      })
+import { qusing2stacks, CmdProcessor } from './solution'
+describe('qusing2stacks', () => {
+  test("Smoke test", () => {
+    const input = [
+      "10", "1 42", "2", "1 14", "3", "1 28", "3", "1 60", "1 78", "2", "2"
+    ]
+    const expected = [
+      "14", "14"
+    ]
+    expect(qusing2stacks(input)).toEqual(expected)
+  })
+  describe("CmdProcessor", () => {
+    it("should exist", () => {
+      expect(CmdProcessor).toBeDefined()
     })
-    
-  
+    describe("When constructed with enqueue, dequeue, and print fns", () => {
+      let cmd
+      beforeEach(() => {
+        cmd = new CmdProcessor({
+          enqueue:jest.fn(),
+          dequeue:jest.fn(),
+          print:jest.fn(),
+        })
+        it("should exist", ()=>{
+          expect(cmd).toBeDefined()
+        })
+      });
+    })
+  })
+})
+
+
--- src/q-using-2-stacks/solution.ts
+++ src/q-using-2-stacks/solution.ts
@@ -1,9 +1,11 @@
+class CmdProcessor {
 
-  function qusing2stacks(input:string[]):string[] {
+}
+function qusing2stacks(input:string[]):string[] {
     return  [
       "14", "14"
     ]
   }
-  export {qusing2stacks}
+  export {qusing2stacks, CmdProcessor}
 
   
